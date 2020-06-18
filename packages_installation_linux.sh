7d06e3e1eb24ff26603f1825b7cc3ae66cdcc168
mjs2369 <mjs2369@columbia.edu>
2020-06-18 14:23:07 -0400

    reducing number of commands and password prompts

--- packages_installation_linux.sh
+++ packages_installation_linux.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install python3-pip
+git clone https://github.com/rzellem/EXOTIC.git
+
+sudo apt-get -y update && sudo apt-get -y upgrade
+apt-get -y install python3-pip
 
 pip3 install numpy
 pip3 install astropy
-sudo pip3 install python-dateutil
+pip3 install python-dateutil
 pip3 install barycorrpy
 pip3 install --upgrade keyrings.alt
 pip3 install matplotlib
 pip3 install pymc3
 pip3 install theano
 pip3 install photutils
-sudo apt-get -y install g++
-python3 exotic.py
+apt-get -y install g++
+python3 ./EXOTIC/exotic.py

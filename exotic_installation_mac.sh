d18deafe9d99ce173684558f1f9a9dee7bcc0f46
mjs2369 <mjs2369@columbia.edu>
2020-06-18 16:22:16 -0400

    completed mac script!

--- exotic_installation_mac.sh
+++ exotic_installation_mac.sh
@@ -6,17 +6,18 @@ git clone https://github.com/rzellem/EXOTIC.git
 ##export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 brew install python
-brew install pip
+#brew install pip
 
-pip install numpy
-pip install astropy
-pip install python-dateutil
-pip install barycorrpy
-pip install --upgrade keyrings.alt
-pip install matplotlib
-pip install pymc3
-pip install theano
-pip install photutils
+pip3 install numpy
+pip3 install astropy
+pip3 install python-dateutil
+pip3 install barycorrpy
+pip3 install --upgrade keyrings.alt
+pip3 install matplotlib
+pip3 install pymc3
+pip3 install theano
+pip3 install photutils
+pip3 install astroalign
 
 #pip -y install g++
-python ./EXOTIC/exotic.py
+python3 ./EXOTIC/exotic.py

e8912cb1621157186155c6a46841dd386c8d7300
mjs2369 <mjs2369@columbia.edu>
2020-06-24 17:46:27 -0400

    addressing feedback and removing git clone from scripts

--- exotic_installation_linux.sh
+++ exotic_installation_linux.sh
@@ -1,23 +1,15 @@
-#!/bin/sh
-
-git clone https://github.com/rzellem/EXOTIC.git
+#!/usr/bin/env sh
 
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt install python3
 sudo apt-get -y install python3-pip
-
-pip3 install numpy
-pip3 install astropy
-pip3 install python-dateutil
-pip3 install barycorrpy
-pip3 install --upgrade keyrings.alt
-pip3 install matplotlib
-pip3 install pymc3
-pip3 install theano
-pip3 install photutils
 sudo apt-get -y install g++
-pip3 install astroalign
+pip3 install --upgrade pip
 
-python3 ./EXOTIC/exotic.py
+
+pip3 install -r requirements.txt
+pip3 install --upgrade keyrings.alt
+
+chmod 755 exotic.py && python3 exotic.py || python exotic.py

fb166833fc1c303befbe8d715ce0729c63c4271f
rzellem <rzellem@jpl.nasa.gov>
2020-08-10 14:25:34 -0700

    v0.15.0: Brazilian Portuguese Instructions
    Features:
    - Brazilian Portuguese (Português do Brasil) translation of the instructions added
    
    Bugs:
    - FITS file path bug fixed for EXOTIC exiting automatically when user left entry blank

--- exotic.py
+++ exotic.py
@@ -29,7 +29,7 @@
 # PATCH version when you make backwards compatible bug fixes.
 # Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
 # https://semver.org
-versionid = "0.14.4"
+versionid = "0.15.0"
 
 
 # --IMPORTS -----------------------------------------------------------

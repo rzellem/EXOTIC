3e0208d7ccc50a398ccd8d6f8d232d5bde537253
jpl-jengelke <jengelke@jpl.nasa.gov>
2020-08-16 23:51:07 -0700

    #209: Add automatic extension of PYTHONPATH (via sys.path) to make application self-sufficient notwithstanding PATH settings.

--- exotic/__init__.py
+++ exotic/__init__.py
@@ -1,4 +1,12 @@
 import importlib_metadata as metadata
+from pathlib import Path
+import sys
+
+# extend PYTHONPATH
+path_update = ['.', str(Path(__file__).resolve().parent)]
+for p in path_update:
+    if p not in sys.path:
+        sys.path.append(p)
 
 try:  # module import
     from .api.versioning import version_read
@@ -18,4 +26,3 @@ except metadata.PackageNotFoundError:
         __version__ = "unknown"
         pass
     pass
-

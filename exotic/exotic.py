927fa3ff28e01c0919531e513f8e9914744abac5
Tamim Fatahi <tamimfatahi@gmail.com>
2020-10-06 12:46:06 -0700

    Updating version.py with comments and removing from exotic.py

--- exotic/exotic.py
+++ exotic/exotic.py
@@ -1,35 +1,3 @@
-# Copyright (c) 2019-2020, California Institute of Technology.
-# All rights reserved.  Based on Government Sponsored Research under contracts NNN12AA01C, NAS7-1407 and/or NAS7-03001.
-
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-#    3. Neither the name of the California Institute of Technology (Caltech), its operating division the Jet Propulsion Laboratory (JPL), the National Aeronautics and Space Administration (NASA), nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE CALIFORNIA INSTITUTE OF TECHNOLOGY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# ########################################################################### #
-# EXOplanet Transit Interpretation Code (EXOTIC)
-#
-# Authors: Ethan Blaser, Rob Zellem, Kyle Pearson, Tamim Fatahi, Marlee Smith,
-#     Aaron Tran, John Engelke, Sujay Nair, Jon Varghese, Michael Fitzgerald
-# Supplemental Code: Kyle Pearson, Gael Roudier, and Jason Eastman
-# ########################################################################### #
-# EXOTIC version number
-# Now adhering to the Semantic Versioning 2.0.0
-# Given a version number MAJOR.MINOR.PATCH, increment the:
-# MAJOR version when you make incompatible API changes,
-# MINOR version when you add functionality in a backwards compatible manner, and
-# PATCH version when you make backwards compatible bug fixes.
-# Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
-# https://semver.org, e.g. __version__ = "0.14.4" from the version file import at ./version.py
-# ########################################################################### #
-
 # -- IMPORTS START ------------------------------------------------------------
 # ########## IMPORTS -- PRELOAD ANIMATION START ##########
 done_flag_animate_exotic = None
@@ -555,10 +523,10 @@ def inits_file(inits_path, dict_info, dict_params):
                        'plate_opt': 'Plate Solution? (y/n)',
                        'pixel_scale': 'Pixel Scale (Ex: 5.21 arcsecs/pixel)'}
 
-    comparison_parameters = {'ra': 'Target Star RA (hh:mm:ss)',
-                             'dec': 'Target Star Dec (+/-hh:mm:ss)',
-                             'pName': "Planet's Name",
-                             'sName': "Host Star's Name",
+    comparison_parameters = {'ra': 'Target Star RA',
+                             'dec': 'Target Star Dec',
+                             'pName': "Planet Name",
+                             'sName': "Host Star Name",
                              'pPer': 'Orbital Period (days)',
                              'pPerUnc': 'Orbital Period Uncertainty',
                              'midT': 'Published Mid-Transit Time (BJD-UTC)',

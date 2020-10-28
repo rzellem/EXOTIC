0122609c6ee61729bc17763c5ae3489d81d52062
mjs2369 <mjs2369@columbia.edu>
2020-10-28 18:44:32 -0400

    fixing structure issues

--- exotic/api/plotting.py
+++ exotic/api/plotting.py
@@ -3,12 +3,12 @@ from io import BytesIO
 from astropy.io import fits
 from astroscrappy import detect_cosmics
 from scipy.ndimage import label
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, save
 from bokeh.palettes import Viridis256
 from bokeh.models import ColorBar, LinearColorMapper, LogColorMapper, LogTicker
 from bokeh.io import output_notebook
 
-def plot_image(filename):
+def plot_image(filename, showorno):
 
     hdu = fits.open(filename)
     print(hdu.info())
@@ -50,13 +50,13 @@ def plot_image(filename):
     ##TODO: add colorbar
 
     # set up a colobar + data range
-    mapper = LinearColorMapper( palette=Viridis256, low=np.percentile(data, 55), high=np.percentile(cdata, 99))
+    color_mapper = LogColorMapper(palette="Viridis256", low=np.percentile(data, 55), high=np.percentile(data, 99))
 
     # must give a vector of image data for image parameter
     fig.image(
         image=[cdata],
           x=0, y=0, dw=hdu[0].data.shape[1], dh=hdu[0].data.shape[0],
-          level="image", color_mapper=mapper
+          level="image", color_mapper=color_mapper
     )
 
     # plot saturated stars
@@ -65,17 +65,14 @@ def plot_image(filename):
 
     fig.grid.grid_line_width = 0.5
 
-    color_mapper = LogColorMapper(palette="Viridis256", low=np.percentile(cdata, 55), high=np.percentile(cdata, 99))
-
     color_bar = ColorBar(color_mapper=color_mapper, ticker=LogTicker(),
                          label_standoff=12, border_line_color=None, location=(0,0))
 
     fig.add_layout(color_bar, 'right')
 
     #output_file("image.html", title="fts example")
-    show(fig)
-
-
-
-##Testing
-plot_image("Kepler412b.fts")
+    if(showorno):
+        show(fig)
+    else:
+        output_file("interactivefits.html")
+        save(fig)

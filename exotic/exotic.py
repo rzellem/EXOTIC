45f024fa7f8f8d58f7c8adefe33f3d7be708289e
Tamim Fatahi <tamimfatahi@gmail.com>
2021-04-22 13:26:24 -0700

    Adding compstarlist

--- exotic/exotic.py
+++ exotic/exotic.py
@@ -1851,6 +1851,7 @@ def main():
 
             inputfiles = inputfiles[inc:]
             wcs_file = check_wcs(inputfiles[0], exotic_infoDict['save'], exotic_infoDict['plate_opt'])
+            compStarList = exotic_infoDict['comp_stars']
 
             if wcs_file:
                 log.info(f"\nHere is the path to your plate solution: {wcs_file}")

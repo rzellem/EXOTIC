1a0be74fb66d5e16a584afba5c4b6a199b46c3df
Tamim Fatahi <tamimfatahi@gmail.com>
2021-08-06 11:08:02 -0700

    Change in obs code inputs

--- exotic/inputs.py
+++ exotic/inputs.py
@@ -280,7 +280,8 @@ def obs_code(code):
     if code is None:
         code = user_input("Please enter your AAVSO Observer Account Number "
                           "(if none, leave blank and press enter): ", type_=str)
-    if code.lower().strip() == 'n/a':
+    code = code.strip()
+    if code.lower() == 'n/a':
         code = ""
     return code
 
@@ -289,7 +290,8 @@ def second_obs_code(code):
     if code is None:
         code = user_input("Please enter your comma-separated Secondary Observer Codes "
                           "(if none, leave blank and press enter): ", type_=str)
-    if code.lower().strip() == 'n/a':
+    code = code.strip()
+    if code.lower() == 'n/a':
         code = ""
     return code
 

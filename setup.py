16119e5bd39f680708dedff9f33fde0e301c88af
jpl-jengelke <jengelke@jpl.nasa.gov>
2022-06-02 05:31:40 -0700

    Issue #992: Migrated to pyproject.toml and config-based build system. This ameliorates deprecated setuptools behaviors and repairs installation steps that require multiple repeated executions of installation steps. Also changed build layout to comport better with standards for locating docs and unit tests.

--- setup.py
+++ setup.py
@@ -1,98 +1,27 @@
 #! /usr/bin/env python3
+# CHECK VERSION:
 # python3 setup.py --version
-# FOR RELEASE:
+# INSTALL TOOLS:
+# pip3 install --upgrade build twine  # https://twine.readthedocs.io/en/latest/
+# CLEAN PREVIOUS BUILDS:
+# rm -r dist *.egg* .egg* build ; pip3 uninstall exotic ; git checkout exotic/version.py # pipenv uninstall exotic
+# RELEASE PRODUCT:
 # (1) manually update exotic/version.py
-# git add exotic/version.py && git commit -m "#<ticket>:Updated version for release" && git push
-# (2) tag using web ui on github master branch 
+# git add exotic/version.py && git commit -m "#<ticket>: Updated version for release" && git push
+# (2) tag using web ui on github master branch
+# or, alternately, git tag -a -m "#<ticket>: Release version <version>" <version>
 # (3) build and package
-# git checkout exotic/version.py && python3 setup.py sdist --format=gztar,zip bdist_wheel --universal  # build
+# git checkout exotic/version.py && python3 -m build --sdist
+# python3 setup.py sdist --format=gztar,zip bdist_wheel --universal  # build
 # (3) release
-# pip3 install twine  # https://twine.readthedocs.io/en/latest/
 # twine check dist/* && twine upload dist/*  # upload
 # FOR LOCAL TESTING:
 # python3 setup.py sdist --format=gztar,zip bdist_wheel --universal  # build
 # pip3 install --exists-action w --progress-bar ascii -r requirements.txt  # install req's
 # pip3 install exotic --no-index --find-links file:///proj/survey-ws/source/EXOTIC/dist/  # install locally
-# CLEAN PREVIOUS BUILDS: 
-# rm -r dist *.egg* .egg* build ; pip3 uninstall exotic ; # pipenv uninstall exotic
-
-import re
-import time
-from pathlib import Path
-
-import setuptools
-
-# Package meta-data sane defaults
-AUTHOR = "Exoplanet Watch at NASA JPL"
-AUTHOR_EMAIL = "exoplanetwatch@jpl.nasa.gov"
-DESCRIPTION = "EXOTIC: EXOplanet Transit Interpretation Code"
-NAME = "exotic"
-PYTHON_REQUIREMENTS = "3.8"
-URL = "https://github.com/rzellem/EXOTIC"
-
-REQUIREMENTS_SETUP = ["setuptools_scm"]
 
 
-def description_read():
-    description_long = ""
-    description_path = Path("README.md")
-    if not description_path.exists():
-        return description_long
-    with description_path.open('r', encoding="utf8") as f:
-        description_long = f.read()
-    return description_long
+from setuptools import setup
 
-
-def license_read():
-    annum = time.gmtime().tm_year
-    # provide one-line summary per LICENSE spec
-    lic = f"Proprietary -- Copyright (c) 2019-{annum}, California Institute of Technology." 
-    return lic
-
-
-setuptools.setup(name=NAME,
-                 use_scm_version={
-                     'write_to': 'exotic/version.py',
-                     'write_to_template': '__version__ = "{version}"'
-                 },
-                 description=DESCRIPTION,
-                 long_description=description_read(),
-                 long_description_content_type='text/markdown',
-                 url=URL,
-                 author=AUTHOR,
-                 author_email=AUTHOR_EMAIL,
-                 license=license_read(),
-                 classifiers=[
-                     # Trove classifiers
-                     # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-                     'Development Status :: 4 - Beta',
-                     'Intended Audience :: End Users/Desktop',
-                     'Intended Audience :: Science/Research',
-                     'License :: Other/Proprietary License',
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 3',
-                     f'Programming Language :: Python :: {PYTHON_REQUIREMENTS}',
-                     'Programming Language :: Python :: Implementation :: CPython',
-                     'Programming Language :: Python :: Implementation :: PyPy',
-                     'Topic :: Scientific/Engineering :: Astronomy'
-                 ],
-                 keywords='nasa jpl exoplanet transit citizen science astronomy bayesian nested-sampler',
-                 project_urls={
-                    "Documentation": "https://github.com/rzellem/EXOTIC/wiki",
-                    "Site": "https://exoplanets.nasa.gov/exoplanet-watch",
-                    "Source": "https://github.com/rzellem/EXOTIC",
-                    "Tracker": "https://github.com/rzellem/EXOTIC/issues"
-                 },
-                 #  https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
-                 packages=setuptools.find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-                 include_package_data=True,
-                 zip_safe=False,
-                 python_requires=f">={PYTHON_REQUIREMENTS}",
-                 setup_requires=REQUIREMENTS_SETUP,
-                 entry_points={
-                     'console_scripts': [
-                         'exotic = exotic.exotic:main',
-                         'exotic-gui = exotic.exotic_gui:main',
-                     ],
-                 }
-                 )
+if __name__ == "__main__":
+    setup()

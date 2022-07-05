86ab546b95f31ec8c04210db029102a2cca9ba6e
Tamim Fatahi <tamimfatahi@gmail.com>
2022-07-05 15:09:11 -0700

    Updating filters.py as well as its usage in other files.

--- exotic/api/filters.py
+++ exotic/api/filters.py
@@ -1,34 +1,46 @@
 # Source for FWHM band wavelengths (units: nm): https://www.aavso.org/filters
 # Near-Infrared
-fwhm = {('J NIR 1.2micron', 'J'): (1040.00, 1360.00), ('H NIR 1.6micron', 'H'): (1420.00, 1780.00),
-        ('K NIR 2.2micron', 'K'): (2015.00, 2385.00),
+fwhm = {
+        "J NIR 1.2 micron": {"name": "J", "desc": "J NIR 1.2micron", "fwhm": ("1040.0", "1360.0")},
+        "H NIR 1.6 micron": {"name": "H", "desc": "H NIR 1.6micron", "fwhm": ("1420.0", "1780.0")},
+        "K NIR 2.2 micron": {"name": "K", "desc": "K NIR 2.2micron", "fwhm": ("2015.0", "2385.0")},
 
         # Sloan
-        ('Sloan u', 'SU'): (321.80, 386.80), ('Sloan g', 'SG'): (402.50, 551.50),
-        ('Sloan r', 'SR'): (553.10, 693.10), ('Sloan i', 'SI'): (697.50, 827.50),
-        ('Sloan z', 'SZ'): (841.20, 978.20),
+        "Sloan u": {"name": "SU", "desc": "Sloan u", "fwhm": ("321.8", "386.8")},
+        "Sloan g": {"name": "SG", "desc": "Sloan g", "fwhm": ("402.5", "551.5")},
+        "Sloan r": {"name": "SR", "desc": "Sloan r", "fwhm": ("553.1", "693.1")},
+        "Sloan i": {"name": "SI", "desc": "Sloan i", "fwhm": ("697.5", "827.5")},
+        "Sloan z": {"name": "SZ", "desc": "Sloan z", "fwhm": ("841.2", "978.2")},
 
         # Stromgren
-        ('Stromgren b', 'STB'): (459.55, 478.05), ('Stromgren y', 'STY'): (536.70, 559.30),
-        ('Stromgren Hbw', 'STHBW'): (481.50, 496.50), ('Stromgren Hbn', 'STHBN'): (487.50, 484.50),
+        "Stromgren b": {"name": "STB", "desc": "Stromgren b", "fwhm": ("459.55", "478.05")},
+        "Stromgren y": {"name": "STY", "desc": "Stromgren y", "fwhm": ("536.7", "559.3")},
+        "Stromgren Hbw": {"name": "STHBW", "desc": "Stromgren Hbw", "fwhm": ("481.5", "496.5")},
+        "Stromgren Hbn": {"name": "STHBN", "desc": "Stromgren Hbn", "fwhm": ("487.5", "484.5")},
 
         # Johnson
-        ('Johnson U', 'U'): (333.80, 398.80), ('Johnson B', 'B'): (391.60, 480.60),
-        ('Johnson V', 'V'): (502.80, 586.80), ('Johnson R', 'RJ'): (590.00, 810.00),
-        ('Johnson I', 'IJ'): (780.00, 1020.00),
+        "Johnson U": {"name": "U", "desc": "Johnson U", "fwhm": ("333.8", "398.8")},
+        "Johnson B": {"name": "B", "desc": "Johnson B", "fwhm": ("391.6", "480.6")},
+        "Johnson V": {"name": "V", "desc": "Johnson V", "fwhm": ("502.8", "586.8")},
+        "Johnson R": {"name": "RJ", "desc": "Johnson R", "fwhm": ("590.0", "810.0")},
+        "Johnson I": {"name": "IJ", "desc": "Johnson I", "fwhm": ("780.0", "1020.0")},
 
         # Cousins
-        ('Cousins R', 'R'): (561.70, 719.70), ('Cousins I', 'I'): (721.00, 875.00),
+        "Cousins R": {"name": "R", "desc": "Cousins R", "fwhm": ("561.7", "719.7")},
+        "Cousins I": {"name": "I", "desc": "Cousins I", "fwhm": ("721.0", "875.0")},
 
         # MObs Clear Filter, Source: Martin Fowler
-        ('MObs CV', 'CV'): (350.00, 850.00),
+        "MObs CV": {"name": "CV", "desc": "MObs CV", "fwhm": ("350.0", "850.0")},
 
         # Astrodon CBB: George Silvis: https://astrodon.com/products/astrodon-exo-planet-filter/
-        ('Astrodon ExoPlanet-BB', 'CBB'): (500.00, 1000.00),
+        "Astrodon ExoPlanet-BB": {"name": "CBB", "desc": "Astrodon ExoPlanet-BB", "fwhm": ("500.0", "1000.0")},
 
         # LCO, Source: Kalee Tock & Michael Fitzgerald, https://lco.global/observatory/instruments/filters/
-        ('LCO Bessell B', 'N/A'): (391.60, 480.60), ('LCO Bessell V', 'N/A'): (502.80, 586.80),
-        ('LCO Bessell U', 'N/A'): (325.0, 375.00), ('Harris B', 'N/A'): (377.75, 484.65),
-        ('LCO Pan-STARRS w', 'N/A'): (404.20, 845.80), ('LCO Pan-STARRS w', 'N/A'): (404.20, 845.80),
-        ('LCO Pan-STARRS zs', 'N/A'): (818.00, 922.00), ('LCO SDSS g\'', 'N/A'): (402.00, 552.00),
-        ('LCO SDSS r\'', 'N/A'): (552.00, 691.00), ('LCO SDSS i\'', 'N/A'): (690.00, 819.00)}
+        "LCO Bessell B": {"name": "N/A", "desc": "LCO Bessell B", "fwhm": ("391.6", "480.6")},
+        "LCO Bessell V": {"name": "N/A", "desc": "LCO Bessell V", "fwhm": ("502.8", "586.8")},
+        "LCO Pan-STARRS w": {"name": "N/A", "desc": "LCO Pan-STARRS w", "fwhm": ("404.2", "845.8")},
+        "LCO Pan-STARRS zs": {"name": "N/A", "desc": "LCO Pan-STARRS zs", "fwhm": ("818.0", "922.0")},
+        "LCO SDSS g'": {"name": "N/A", "desc": "LCO SDSS g'", "fwhm": ("402.0", "552.0")},
+        "LCO SDSS r'": {"name": "N/A", "desc": "LCO SDSS r'", "fwhm": ("552.0", "691.0")},
+        "LCO SDSS i'": {"name": "N/A", "desc": "LCO SDSS i'", "fwhm": ("690.0", "819.0")}
+}

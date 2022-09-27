74977c939787f4252c6eec2ed8158b3d5e239f22
jpl-jengelke <jengelke@jpl.nasa.gov>
2022-09-26 18:20:24 -0700

    Issue #971, #988: Updates to support modern build tooling. ...

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
+# INSTALL TOOLS:
+# pip3 install -r requirements.txt ; pip3 install --upgrade build twine  # https://twine.readthedocs.io/en/latest/
+# CLEAN PREVIOUS BUILDS:
+# rm -r build dist __pycache__ *.egg* .egg* ; pip3 uninstall exotic ; git checkout exotic/version.py # pipenv uninstall exotic
 # CHECK VERSION:
 # python3 setup.py --version
-# INSTALL TOOLS:
-# pip3 install --upgrade build twine  # https://twine.readthedocs.io/en/latest/
-# CLEAN PREVIOUS BUILDS:
-# rm -r dist *.egg* .egg* build ; pip3 uninstall exotic ; git checkout exotic/version.py # pipenv uninstall exotic
 # RELEASE PRODUCT:
+# ############################## RELEASE PRODUCT  ##############################
 # (1) manually update exotic/version.py
 # git add exotic/version.py && git commit -m "#<ticket>: Updated version for release" && git push
 # (2) tag using web ui on github master branch
@@ -15,9 +16,9 @@
 # python3 setup.py sdist --format=gztar,zip bdist_wheel --universal  # build
 # (3) release
 # twine check dist/* && twine upload dist/*  # upload
-# FOR LOCAL TESTING:
+# ############################### LOCAL TESTING  ###############################
 # python3 setup.py sdist --format=gztar,zip bdist_wheel --universal  # build
-# pip3 install --exists-action w --progress-bar ascii -r requirements.txt  # install req's
+# pip3 --exists-action w install -r requirements.txt  # install req's
 # pip3 install exotic --no-index --find-links file:///proj/survey-ws/source/EXOTIC/dist/  # install locally
 
 

ebac01bcd61f482e4874822cb1a97da41e94ae93
John Engelke <52300928+jpl-jengelke@users.noreply.github.com>
2023-01-18 09:00:10 -0800

    Issue #1131: Minor fixes in support of Windows installation. ...

--- run_exotic_windows.bat
+++ run_exotic_windows.bat
@@ -1,7 +1,7 @@
 ECHO OFF
 
-pip install wheel
-pip install setuptools
-pip install --upgrade python-certifi-win32
+pip install "importlib-metadata>=3.6;python_version<='3.7'" oldest-supported-numpy setuptools>=62.6 setuptools_scm[toml]>=6.4.2 wheel>=0.37.1
+# pip install --upgrade python-certifi-win32
 pip install --upgrade exotic
+
 start cmd /k exotic-gui

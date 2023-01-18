93bc9d70dbc1f561460991a3a3c47de54a2b5944
John Engelke <52300928+jpl-jengelke@users.noreply.github.com>
2023-01-18 09:03:15 -0800

    Issue #1131: Added line to report Pip version. ...

--- run_exotic_windows.bat
+++ run_exotic_windows.bat
@@ -1,5 +1,6 @@
 ECHO OFF
 
+pip --version
 pip install "importlib-metadata>=3.6;python_version<='3.7'" oldest-supported-numpy setuptools>=62.6 setuptools_scm[toml]>=6.4.2 wheel>=0.37.1
 # pip install --upgrade python-certifi-win32
 pip install --upgrade exotic

75a2565d859898558af0af41ee72920ea8b3c251
Michael Primm <mprimm@mirificsystems.com>
2023-03-23 14:46:07 -0500

    Add comments for new fields

--- exotic/exotic_gui.py
+++ exotic/exotic_gui.py
@@ -362,7 +362,7 @@ def main():
                 "Target Star RA": "Must be in HH:MM:SS sexagesimal format.",
                 "Target Star DEC": "Must be in +/-DD:MM:SS sexagesimal format with correct sign at the beginning (+ or -).",
                 'Demosaic Format': "Optional control for handling Bayer pattern color images - to use, provide Bayer color patttern of your camera (RGGB, BGGR, GRBG, GBRG) - null (no color processing) is default",
-                'Demosaic Output': "Select how to process color data (gray for grayscale, red or green or blue for single color channel, blueblock for grayscale without blue, [ R, G, B ] for custom weights for mixing colors.  green is default"
+                'Demosaic Output': "Select how to process color data (gray for grayscale, red or green or blue for single color channel, blueblock for grayscale without blue, [ R, G, B ] for custom weights for mixing colors.  green is default",
                 "Formatting of null": "Due to the file being a .json, null is case sensitive and must be spelled as shown.",
                 "Decimal Format": "Leading zero must be included when appropriate (Ex: 0.32, .32 or 00.32 causes errors.)."
             }

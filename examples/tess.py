8b8c4ff6c48fe0f875cf941bc450ed23f7b3aab2
Kyle A Pearson <kyle.a.pearson@jpl.nasa.gov>
2023-06-21 11:48:18 -0700

    tess instructions

--- examples/tess.py
+++ examples/tess.py
@@ -2,12 +2,10 @@
 # git clone https://github.com/rzellem/EXOTIC.git
 # cd EXOTIC
 # git checkout tess
-# cd examples
 # conda create -n tess python=3.9
 # conda activate tess
 # pip install pandas scipy matplotlib astropy statsmodels cython numpy==1.21.6
 # pip install wotan transitleastsquares pylightcurve lightkurve==2.0.6 ultranest==3.5.6
-# cd ..
 # pip install .
 # cd examples
 # python tess.py -t "HAT-P-18 b"

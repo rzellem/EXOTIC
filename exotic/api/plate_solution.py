30cafce9243f219a2d4d821d55063447b1eca9a7
jpl-jengelke <jengelke@jpl.nasa.gov>
2024-08-12 22:21:43 -0700

    Issue #1264: Re-add small-ish timeout to requests operations. ...

--- exotic/api/plate_solution.py
+++ exotic/api/plate_solution.py
@@ -45,6 +45,7 @@ from tenacity import retry, retry_if_exception_type, retry_if_result, \
 _R_MAX_STOPS_LOW = 7
 _R_MAX_STOPS = 10
 _R_MAX_SECS = 37
+_RQ_TIMEOUT = 16.0
 
 
 def is_false(value):
@@ -97,7 +98,7 @@ class PlateSolution:
            retry=(retry_if_result(is_false) | retry_if_exception_type(requests.exceptions.RequestException)),
            retry_error_callback=result_if_max_retry_count)
     def _login(self):
-        r = requests.post(self._get_url('login'), data={'request-json': dumps(self.api_key)})
+        r = requests.post(self._get_url('login'), data={'request-json': dumps(self.api_key)}, timeout=_RQ_TIMEOUT)
         if r.status_code >= 400:
             return False
         elif r.json()['status'] == 'success':
@@ -112,7 +113,7 @@ class PlateSolution:
         headers = {'request-json': dumps({"session": session}), 'allow_commercial_use': 'n',
                    'allow_modifications': 'n', 'publicly_visible': 'n'}
 
-        r = requests.post(self.api_url + 'upload', files=files, data=headers)
+        r = requests.post(self.api_url + 'upload', files=files, data=headers, timeout=_RQ_TIMEOUT)
 
         if r.json()['status'] == 'success':
             return r.json()['subid']
@@ -122,7 +123,7 @@ class PlateSolution:
            retry=(retry_if_result(is_false) | retry_if_exception_type(requests.exceptions.RequestException)),
            retry_error_callback=result_if_max_retry_count)
     def _sub_status(self, sub_url):
-        r = requests.get(sub_url)
+        r = requests.get(sub_url, timeout=_RQ_TIMEOUT)
         if r.json()['job_calibrations']:
             return r.json()['jobs'][0]
         return False
@@ -131,9 +132,9 @@ class PlateSolution:
            retry=(retry_if_result(is_false) | retry_if_exception_type(requests.exceptions.RequestException)),
            retry_error_callback=result_if_max_retry_count)
     def _job_status(self, job_url, wcs_file, download_url):
-        r = requests.get(job_url)
+        r = requests.get(job_url, timeout=_RQ_TIMEOUT)
         if r.json()['status'] == 'success':
-            r = requests.get(download_url)
+            r = requests.get(download_url, timeout=_RQ_TIMEOUT)
             with wcs_file.open('wb') as f:
                 f.write(r.content)
             hdu = PrimaryHDU(data=getdata(filename=self.file), header=getheader(filename=wcs_file))
